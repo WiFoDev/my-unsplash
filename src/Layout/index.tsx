cfc4821fd911d73846448494d9913ea3a3651133
WiFoDev <lou81804@gmail.com>
2022-11-20 11:32:20 -0500

    update page header layout

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,5 +1,9 @@
 import Head from "next/head";
+import Image from "next/image";
 import React from "react";
+
+import unsplashLogo from "@/assets/my_unsplash_logo.svg";
+import searchIcon from "@/assets/search-icon.svg";
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -9,14 +13,28 @@ export const Layout = ({children}: LayoutProps) => {
   return (
     <>
       <Head>
-        <title>APP</title>
-        <meta content="The next generation app" name="My App" />
+        <title>My Unsplash</title>
+        <meta content="The next unsplash app" name="My Unsplash" />
         <link href="/favicon.ico" rel="icon" />
       </Head>
       <header className="sticky top-0 z-20 w-full">
-        <nav className="flex pointer-events-none h-16 mx-auto max-w-screen-standar items-center justify-end gap-2 pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)]">
-          <div className="mr-auto text-xl">My APP</div>
-          <ul className="text-lg">Navigation List For The Page</ul>
+        <nav className="flex h-16 mx-auto max-w-screen-standar items-center justify-end gap-2 pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)]">
+          <div className="flex items-center gap-4 mr-auto">
+            <Image alt="My unsplash logo" src={unsplashLogo} />
+            <label className="flex items-center gap-2 border-[1px] border-secondary p-3 text-xs rounded-md">
+              <div className="w-4 h-4">
+                <Image alt="Search Icon" src={searchIcon} />
+              </div>
+              <input
+                className="outline-none"
+                placeholder="Search by name"
+                type="text"
+              />
+            </label>
+          </div>
+          <button className="px-4 py-3 text-sm font-semibold text-white rounded-lg shadow-md shadow-primary/50 bg-primary">
+            Add a photo
+          </button>
         </nav>
       </header>
       <main className="relative flex flex-col items-center justify-center w-full h-full">
