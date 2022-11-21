de04a133c1de0394617130f30fc7a66cff220ab0
WiFoDev <lou81804@gmail.com>
2022-11-21 16:30:14 -0500

    update upload to cloudinary functionality

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,12 +1,26 @@
 import {NextApiRequest, NextApiResponse} from "next";
+import {UploadApiOptions, v2 as cloudinary} from "cloudinary";
 
-export default function handler(
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+  secure: true,
+});
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   const {label, photoURL} = req.body;
+  const options: UploadApiOptions = {
+    folder: "my-unsplash",
+  };
 
-  console.log(label, photoURL);
+  const {secure_url} = await cloudinary.uploader.upload(
+    photoURL,
+    options,
+  );
 
-  return res.status(200).json({success: true});
+  return res.status(200).json({success: true, secure_url});
 }
